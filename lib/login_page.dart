7c42b028f88958646633d09ca03fd9e110a8c415
gpffhajdzl <gpffhajdzl3205@gmail.com>
2020-04-24 15:22:43 +0900

    Create login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_builder.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Login Page')
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Instagram Clon',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: EdgeInsets.all(50.0),
+              ),
+              SignInButton(
+                Buttons.Google,
+                onPressed: (){
+
+                },
+              )
+            ],
+          ),
+        ),
     );
   }
 }
