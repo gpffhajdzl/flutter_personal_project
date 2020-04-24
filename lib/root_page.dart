89d169075fa96d21c48891009b03717dae45c87c
gpffhajdzl <gpffhajdzl3205@gmail.com>
2020-04-24 19:21:20 +0900

    Complete root_page setting 18:35

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:instagramclon/login_page.dart';
 import 'package:instagramclon/tab_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return StreamBuilder<FirebaseUser>(
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return TabPage(snapshot.data);
+        } else {
+          return LoginPage();
+        }
+      },
+    );
   }
 }
